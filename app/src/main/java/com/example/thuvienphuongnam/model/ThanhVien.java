75faa9053fecdedad2b235b058a8fe90c068c374
Son <sonnvph20319@fpt.edu.vn>
2023-07-12 09:00:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thuvienphuongnam/model/ThanhVien.java
@@ -0,0 +1,17 @@
+package com.example.thuvienphuongnam.model;
+
+public class ThanhVien {
+    public int maTV;
+    public String hoTen;
+    public String namSinh;
+
+    public ThanhVien() {
+    }
+
+    public ThanhVien(int maTV, String hoTen, String namSinh) {
+        this.maTV = maTV;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+    }
+
+}
